7a66c0e4af670165a7687a96072fe6901034550a
Miguel Parramon <miguel@ztory.com>
2014-10-10 17:53:12 +0200

    Add Dir to Users

--- kookie/pure_ftpd/models/user.rb
+++ kookie/pure_ftpd/models/user.rb
@@ -1,6 +1,10 @@
 class User < Sequel::Model(:users)
 
   def self.create(username, password)
-    insert(:User => username, :Password => Digest::MD5.hexdigest(password))
+    insert(
+      User: username,
+      Password: Digest::MD5.hexdigest(password),
+      Dir: "/home/ftp-accounts/#{username}"
+    )
   end
 end
